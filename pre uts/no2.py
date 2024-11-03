a12b66d3f6f5bd6676c38934f6fc18f901e917f1
Kentangz <adhitsholawat12@gmail.com>
2024-11-03 22:16:30 +0700

    Update(2) no2

--- pre uts/no2.py
+++ pre uts/no2.py
@@ -3,6 +3,7 @@
 #     sum
 # print (sum)
 
+
 # a. Apakah output dari code tersebut jika my_list = [1, 2, 3, 4, 5] ?
 my_list = [ 1,2,3,4,5]
 sum = 0
@@ -11,26 +12,37 @@ for x in my_list:
     sum += x
 print(sum)
 
+
 # b. Tambahkan fungsi untuk memberikan output pangkat dan double/kali dua, untuk setiap nilai dari my_list!
 my_list = [1, 2, 3, 4, 5]
 
-squares = [x ** 2 for x in my_list]
-doubles = [x * 2 for x in my_list]
+def pangkat(x):
+    return x ** 2
+def kali(x):
+    return x * 2
+
+squares = list(map(pangkat, my_list))
+doubles = list(map(kali, my_list))
 
 print("Squares:", squares)
 print("Doubles:", doubles)
-
 
 
 # c. Ubah code tersebut menggunakan gaya penulisan fungsional, dengan mengimplementasikan functools, lambda, list, map.
 from functools import reduce
 
 my_list = [1, 2, 3, 4, 5]
-total = reduce(lambda acc, x: acc + x, my_list)
+
+total = reduce(lambda var, x: var + x, my_list)
 
 squares = list(map(lambda x: x ** 2, my_list))
 doubles = list(map(lambda x: x * 2, my_list))
 
 print("Total:", total)
+print("Squares:", squares)
+print("Doubles:", doubles)
+
+
+print(" \n Total:", total)
 print("Squares:", squares)
 print("Doubles:", doubles)
